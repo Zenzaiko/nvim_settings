6dc0c34be59e279abb1b1335b9acbf7f913bd160
Hiroaki Kashimoto <ubuntu@PTPC210007.toyo-local.co.jp>
2024-05-07 19:23:23 +0900

    update keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -18,15 +18,26 @@ keymap.set("i", "jk", "<ESC>")
 keymap.set("n", "<C-a>", "gg<S-v>G")
 
 -- New Tab
-keymap.set("n", "te", "tabedit", opts)
-keymap.set("n", "<tab>", ":tabnext<Retern>", opts)
+keymap.set("n", "te", ":tabedit", opts)
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
 keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
 
 -- Split Window
 keymap.set("n", "ss", ":split<Return>", opts)
-keymap.set("n", "sv", ":vplit<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
 -- Move Window
 keymap.set("n", "sh", "<C-w>h")
 keymap.set("n", "sk", "<C-w>k")
 keymap.set("n", "sj", "<C-w>j")
 keymap.set("n", "sl", "<C-w>l")
+
+-- Resize Window
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
+
+-- Diagnostics
+keymap.set("n", "<C-j>", function()
+  vim.diagnostic.goto_next()
+end, opts)
