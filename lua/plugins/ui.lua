ac17ec023c31ea5f178888c4567663b9a33ce22b
Hiroaki Kashimoto <kashimo@HiroakinoMacBook-Air.local>
2024-05-09 09:09:39 +0900

    lsp treesitter ui install

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      })
+
+      opts.presets.lsp_doc_border = true
+    end,
+  },
+
+  -- display time of message
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 10000,
+    },
+  },
+
+  -- animations
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.scroll = {
+        enable = false,
+      }
+    end,
+  },
+}
